329c27b34324e6192739937afd70b492ed9d6d0c
Faris Ansari <netchamp.faris@gmail.com>
2017-09-24 23:10:47 +0530

    Add dependencies, styling
    - Add sass
    - Add jquery
    - Add bootstrap 4
    - Import bootstrap source files in style.scss

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,16 @@ const config = {
         test: /(\.jsx|\.js)$/,
         loader: 'eslint-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader' // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader' // translates CSS into CommonJS
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
       }
     ]
   },
