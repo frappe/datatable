98afce65ac1ef253ae98064e03b5b1537e19b8f8
Faris Ansari <netchamp.faris@gmail.com>
2018-04-15 22:20:18 +0530

    Refactor rollup config
    Add config for
    - production
    - docs
    
    Add eslint
    
    Remove unused dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,52 +1,108 @@
 import json from 'rollup-plugin-json';
-// import uglify from 'rollup-plugin-uglify';
+import uglify from 'rollup-plugin-uglify-es';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
 import nested from 'postcss-nested';
 import cssnext from 'postcss-cssnext';
-// import cssnano from 'cssnano';
+import eslint from 'rollup-plugin-eslint';
+import merge from 'deepmerge';
 
-const dev = {
+const production = process.argv[3] === '--production';
+
+const baseJS = {
     input: 'src/index.js',
-    output: [{
-        file: 'dist/frappe-datatable.js',
-        format: 'iife',
-        name: 'DataTable',
+    output: {
+        file: '',
         globals: {
             sortablejs: 'Sortable',
             'clusterize.js': 'Clusterize'
         }
-    }, {
-        file: 'docs/assets/frappe-datatable.js',
-        format: 'iife',
-        name: 'DataTable',
-        globals: {
-            sortablejs: 'Sortable',
-            'clusterize.js': 'Clusterize'
-        }
-    }],
+    },
     plugins: [
         json(),
+        eslint({
+            exclude: '**/*.json'
+        }),
         nodeResolve(),
-        commonjs(),
+        commonjs()
+    ],
+    external: ['sortablejs', 'clusterize.js']
+};
+
+const baseCSS = {
+    input: 'src/style.css',
+    output: {
+        file: ''
+    },
+    plugins: [
         postcss({
-            extract: 'dist/frappe-datatable.css',
+            extract: true,
+            minimize: production,
             plugins: [
                 nested(),
                 cssnext()
             ]
         })
-    ],
-    external: ['sortablejs', 'clusterize.js']
+    ]
 };
 
-export default [
-    dev,
-    Object.assign({}, dev, {
-        output: {
-            format: 'cjs',
-            file: 'dist/frappe-datatable.cjs.js'
-        }
-    })
-];
+const devIIFE = merge(baseJS, {
+    output: {
+        file: 'dist/frappe-datatable.js',
+        format: 'iife',
+        name: 'DataTable'
+    }
+});
+
+const devCjs = merge(baseJS, {
+    output: {
+        file: 'dist/frappe-datatable.cjs.js',
+        format: 'cjs'
+    }
+});
+
+const devCSS = merge(baseCSS, {
+    output: {
+        file: 'dist/frappe-datatable.css',
+        format: 'cjs'
+    }
+});
+
+// production
+const prodIIFE = merge(devIIFE, {
+    output: {
+        file: 'dist/frappe-datatable.min.js'
+    },
+    plugins: [
+        uglify()
+    ]
+});
+
+const prodCSS = merge(devCSS, {
+    output: {
+        file: 'dist/frappe-datatable.min.css'
+    }
+});
+
+// docs
+const docJS = merge(devIIFE, {
+    output: {
+        file: 'docs/assets/frappe-datatable.js'
+    }
+});
+
+const docCSS = merge(devCSS, {
+    output: {
+        file: 'docs/assets/frappe-datatable.css'
+    }
+});
+
+const developmentAssets = [devIIFE, devCjs, devCSS];
+const documentationAssets = [docJS, docCSS];
+const productionAssets = [prodIIFE, prodCSS];
+
+const docs = process.argv[3] === '--docs';
+const assets = docs ? documentationAssets : production ? productionAssets : developmentAssets;
+
+export default assets;
