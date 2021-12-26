be11d2b0c907731fe273c663d01ebf06cf04b768
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2021-12-26 19:38:16 +0100

    feat: french and italian translations (#1)
    * Create fr.json
    
    * feat: italian translations
    
    * feat: add "fr" and "it" to getTranslationsJSON

--- src/translations/index.js
+++ src/translations/index.js
@@ -1,9 +1,13 @@
 import en from './en.json';
 import de from './de.json';
+import fr from './fr.json';
+import it from './it.json';
 
 export default function getTranslationsJSON() {
     return {
         en: en,
-        de: de
+        de: de,
+        fr: fr,
+        it: it,
     };
 };
